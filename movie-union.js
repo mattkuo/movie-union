eb24063ede2948807719cede1164ee30c8ed8a87
Matthew Kuo <matthewkuo92@gmail.com>
2015-05-03 03:03:03 -0700

    Record movie id after click, input text update

--- movie-union.js
+++ movie-union.js
@@ -7,39 +7,45 @@ if (Meteor.isClient) {
 
   Template.search.created = function() {
     this.searchResult = new Blaze.ReactiveVar();
+    this.selectedItem = new Blaze.ReactiveVar();
   }
 
   Template.search.events({
     'keyup input': _.throttle(function(ev, template) {
       // Call api here
-      var query = ev.target.value;
+      var query = ev.target.value.trim();
+      if (!query || query.length < 2) return;
       Meteor.call('getMovieData', query, function(err, result) {
         template.searchResult.set(result);
       });
 
     }, 800, {leading: false}),
 
-    'focus input': function(event, template) {
+    'focus input': function(ev, template) {
       var dropdown = template.find('ul.search-results');
 
+    },
+
+    'click li.movie-item': function(ev, template) {
+      var target = ev.currentTarget;
+      var movieId = target.getAttribute('data-movie-id');
+      template.selectedItem.set(movieId);
+      template.find('input').value = target.querySelector('span.title').textContent;
     }
   });
 
   Template.search.helpers({
     movies: function() {
       return Template.instance().searchResult.get();
+    },
+    base_url: function() {
+      return Session.get('base_url');
     }
-  })
+  });
 
   Template.body.events({
     'click button': function() {
-      console.log('fired');
+      // Perform union on movie actors
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
